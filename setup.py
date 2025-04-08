74bf4b48ab960ab4a5915156371f3443fac552c2
enciyo <enciyomk61@gmail.com>
2025-04-08 13:54:28 +0300

    Initial Grok Api

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='grok_unoffical_api',
+    version='0.1.0',
+    description='Unofficial Grok API',
+    author='Mustafa K',
+    author_email='ben@mustafakilic.dev',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'python-dotenv'
+    ],
+)
